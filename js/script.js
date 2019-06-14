26df94178d8af068812ff16fabafaf8c4dfb21f6
Brad Schmitz <schmitb3@miamioh.edu>
2019-06-13 20:47:19 -0400

    Implemented hamburger menu functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-// JavaScript Document
+// On document ready, prepare jQuery
+$(document).ready(function(){
+	
+	// =============================== Toggle menu on hamburger click
+	$("#bars").on("click", function(){
+	
+		// Get sidenav width
+		var width = $("#sidenav").css("width");
+
+		// If sidenav is 0, open; else, close
+		if (width == "0px") {
+			$("#sidenav").css("width", "50%");
+		} else {
+			$("#sidenav").css("width", "0px");
+		}
+	})
+	
+	// =============================== On window resize...
+	$(window).on("resize", function() {
+		
+		// If window is greater than 500px, collapse sidenav
+		if ($(window).width() > 500) {
+			$("#sidenav").css("width", "0px");
+		}
+	})
+
+});
