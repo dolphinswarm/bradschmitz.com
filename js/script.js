b47ebef690074271cc67b70f98bb20212ebc7876
Brad Schmitz <schmitb3@miamioh.edu>
2019-06-19 21:50:25 -0400

    Minor tweaks to the footer

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ $(document).ready(function(){
 
 		// If sidenav is 0, open; else, close
 		if (width == "0px") {
-			$("#sidenav").css("width", "50%");
+			$("#sidenav").css("width", "60%");
+			$("#sidenav").css("border-right", "2px solid #D1D1D1");
 		} else {
 			$("#sidenav").css("width", "0px");
+			$("#sidenav").css("border-right", "0px solid #D1D1D1");
 		}
 		
 		// Toggle animation for open / close
@@ -24,6 +26,7 @@ $(document).ready(function(){
 		// If window is greater than 500px, collapse sidenav
 		if ($(window).width() > 500) {
 			$("#sidenav").css("width", "0px");
+			$("#sidenav").css("border-right", "0px solid #D1D1D1");
 		}
 	})
 
